acade5accf0f851a890ec7609d34513d677b2854
elrikikiki <santizanini1@gmail.com>
2023-03-07 10:48:59 -0300

    form

--- origen.js
+++ origen.js
@@ -1,41 +1,39 @@
-var currentFig = 1;
-var currentDeg = 0;
+function isMobile() {
+  if (sessionStorage.desktop)
+      return false;
+  else if (localStorage.mobile)
+      return true;
+  var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+  for (var i in mobile)
+      if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+  return false;
+}
 
-$(document).ready(function() {
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '3513508302';
 
-  $('#next').click(function(){
-    $('#fig'+currentFig).removeClass('show');
-    currentFig++;
-    if(currentFig>8){
-      currentFig=1;
-    }
-    currentDeg-=45;
-    $('#fig'+currentFig).addClass('show');
-    $('.carousel').css({
-      "-webkit-transform": "rotateY("+currentDeg+"deg)",
-      "-moz-transform": "rotateY("+currentDeg+"deg)",
-      "-ms-transform": "rotateY("+currentDeg+"deg)",
-      "-o-transform": "rotateY("+currentDeg+"deg)",
-      "transform": "rotateY("+currentDeg+"deg)"
-    });
-  });
-
-  $('#prev').click(function(){
-    $('#fig'+currentFig).removeClass('show');
-    currentFig--;
-    if(currentFig<1){
-      currentFig=8;
-    }
-    currentDeg+=45;
-    $('#fig'+currentFig).addClass('show');
-    $('.carousel').css({
-      "-webkit-transform": "rotateY("+currentDeg+"deg)",
-      "-moz-transform": "rotateY("+currentDeg+"deg)",
-      "-ms-transform": "rotateY("+currentDeg+"deg)",
-      "-o-transform": "rotateY("+currentDeg+"deg)",
-      "transform": "rotateY("+currentDeg+"deg)"
-    });
-  });
-
-
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault()
+  buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+  buttonSubmit.disabled = true
+  setTimeout(() => {
+      let nombre = document.querySelector('#nombre').value
+      let apellidos = document.querySelector('#apellidos').value
+      let email = document.querySelector('#email').value
+      let cantPersonas = document.querySelector('#cantPersonas').value
+      let celular = document.querySelector('#celular').value
+      let localidad = document.querySelector('#localidad').value
+      let mensaje = 'send?phone=' + telefono + '&text=*_Formulario Easy App CODE_*%0A*¿Cual es tu nombre?*%0A' + nombre + '%0A%0A' + apellidos + '%0A%0A' + email  + '%0A%0A'
+      + cantPersonas + '%0A%0A' + celular + '%0A%0A' + localidad + '%0A%0A'
+      if(isMobile()) {
+          window.open(urlMobile + mensaje, '_blank')
+      }else{
+          window.open(urlDesktop + mensaje, '_blank')
+      }
+      buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+      buttonSubmit.disabled = false
+  }, 3000);
 });
